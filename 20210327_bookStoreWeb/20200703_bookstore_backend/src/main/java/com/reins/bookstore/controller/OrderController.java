aa4f2e0701004e47b2c10b2724b4812737f42fd6
echo-xiao9 <819601193@qq.com>
2021-10-22 21:18:11 +0800

    finish hw4

---
+++ 20210327_bookStoreWeb/20200703_bookstore_backend/src/main/java/com/reins/bookstore/controller/OrderController.java
@@ -0,0 +1,139 @@
+package com.reins.bookstore.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.reins.bookstore.entity.*;
+import com.reins.bookstore.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class OrderController {
+    @Autowired
+    WebApplicationContext applicationContext;
+    @Autowired
+    private OrderService orderService;
+
+
+    @GetMapping("/getOrders")
+    public String getOrders() {
+//        return orderService.getOrders();
+
+        return JSON.toJSONString(orderService.getOrders(), SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+
+
+    @GetMapping("/getUserOrders")
+    public String getUserOrders(@RequestParam(required = false) Integer user_id){
+        return JSON.toJSONString(orderService.getUserOrders(user_id), SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+    @GetMapping("/getUserOrders1")
+    public java.util.List<Order> getUserOrders1(@RequestParam(required = false) Integer user_id){
+        return orderService.getUserOrders(user_id);
+//        return JSON.toJSONString(orderService.getUserOrders(user_id), SerializerFeature.DisableCircularReferenceDetect);
+    }
+    @GetMapping("/getOrderItem")
+    public List<OrderItem> getOrderItems(@RequestParam(required = false) Integer order_id){
+        return orderService.getOrderItems(order_id);
+    }
+
+    @GetMapping("/getUserBookOrders")
+    public String getUserBookOrders(@RequestParam(required = false)Integer user_id,
+                                    @RequestParam(required = false)String bookName){
+        return JSON.toJSONString(orderService.getUserBookOrders(user_id,bookName),SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+    @GetMapping("/getAdminBookOrders")
+    public String getAdminBookOrders(
+                                    @RequestParam(required = false)String bookName){
+        return JSON.toJSONString(orderService.getAdminBookOrders(bookName),SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+
+    @GetMapping("/getUserDateOrder")
+    public String getUserDateOrder(@RequestParam(required = false)Integer user_id,
+                                    @RequestParam(required = false)String from,
+                                    @RequestParam(required = false)String to
+    ){
+        return JSON.toJSONString(orderService.getUserDateOrder(user_id,from,to),SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+    @GetMapping("/getAdminDateOrder")
+    public String getAdminDateOrder(
+                                   @RequestParam(required = false)String from,
+                                   @RequestParam(required = false)String to
+    ){
+        return JSON.toJSONString(orderService.getAdminDateOrder(from,to),SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+    @GetMapping("/addOrderFromUser")
+    public Order addOrderFromUser (@RequestParam(required = false) Integer user_id,
+                                   @RequestParam (required = false) Integer order_price,
+                                   @RequestParam(required = false)String date
+    ) {
+        JmsTemplate jmsTemplate = applicationContext.getBean(JmsTemplate.class);
+        System.out.println("Sending an order message.");
+        Order order = new Order(user_id,order_price, date);
+        jmsTemplate.convertAndSend("orderBox", new Order(user_id,order_price, date));
+        return order;
+    }
+
+    @GetMapping("/addFullOrder")
+    public Order addFullOrder(@RequestParam(required = false) Integer user_id,
+                              @RequestParam (required = false) Integer order_price,
+                              @RequestParam(required = false)String date
+    ) {
+        return orderService.addFullOrder(new Order(user_id,order_price, date));
+    }
+
+
+
+
+    @GetMapping("/addOrderItem")
+    public OrderItem addOrderItem(
+            @RequestParam(required = false)  Integer order_id,
+            @RequestParam(required = false)  Integer book_id,
+            @RequestParam(required = false)  Integer book_num
+
+    ){
+
+        return orderService.addOrderItem(order_id, book_id,book_num);
+    }
+// can be comment
+
+    @GetMapping("/getHotSelling")
+    public List<HotSelling>  getHotSelling(
+            @RequestParam String from,
+            @RequestParam String to
+    ){
+        return orderService.getHotSelling(from, to);
+    }
+//
+    @GetMapping("/getHotUsers")
+    public List<HotSelling> getHotUsers(
+        @RequestParam String from,
+        @RequestParam String to
+    ){
+        return orderService.getHotUsers(from, to);
+    }
+//
+    @GetMapping("/getUserHotSelling")
+    public UserHotSelling getUserHotSelling(
+            @RequestParam String from,
+            @RequestParam String to,
+            @RequestParam Integer user_id
+    ){
+        return orderService.getUserHotSelling(from, to, user_id);
+    }
+
+
+
+}
---
+++ 20210327_bookStoreWeb/20200703_bookstore_backend/src/main/java/com/reins/bookstore/entity/OrderItem.java
@@ -0,0 +1,73 @@
+package com.reins.bookstore.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.reins.bookstore.repository.BookRepository;
+import com.sun.istack.NotNull;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+@Table(name = "order_items")
+//@JsonIgnoreProperties(value = {"handler","hibernateLazyInitializer","fieldHandler"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "itemId")
+public class OrderItem {
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer itemId;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(ignoreUnknown = true, value = {"orderItemList"})
+    @JoinColumn(name = "order_id")
+    @JSONField(serialize = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    @NotNull
+    @JsonIgnoreProperties(ignoreUnknown = true, value = {"orderItemList"})
+    private Book book;
+
+    private Integer book_num;
+
+    public Integer getBook_num() {
+        return book_num;
+    }
+
+    public Book getBook(){
+        return book;
+    }
+
+    public OrderItem(Order order, Book book, Integer book_num) {
+        this.order = order;
+        this.book = book;
+        this.book_num = book_num;
+    }
+
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Integer order_id, Integer book_id, Integer book_num) {
+        this.book_num=book_num;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
